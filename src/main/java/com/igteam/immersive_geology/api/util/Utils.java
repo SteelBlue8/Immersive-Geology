b26d4e99d42be34a26914e5abe0fb50633bd882f
UnSchtalch <shadow.93.04@gmail.com>
2021-11-10 00:20:41 +0300

    Util functions for coordinate fixer

--- src/main/java/com/igteam/immersive_geology/api/util/Utils.java
+++ src/main/java/com/igteam/immersive_geology/api/util/Utils.java
@@ -1,5 +1,7 @@
 package com.igteam.immersive_geology.api.util;
 
+import java.util.Arrays;
+
 /**
  * Created by Pabilo8 on 26-03-2020.
  */
@@ -14,4 +16,25 @@ public class Utils
     public static final int PLATE_AMOUNT = 144;
     public static final int GEAR_AMOUNT = 576;
 
+    public static double fixCoordinate(double f)
+    {
+        return (Math.round(f*16) )/ 16.0f;
+    }
+
+    public static boolean isVertexLine(String s)
+    {
+        return (s.startsWith("v "));
+    }
+
+    public static String fixVertexString (String s)
+    {
+        if (!isVertexLine(s)) return s;
+        String[] parts = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        result.append("v ");
+        for (int i = 1; i<parts.length; i++) {
+            result.append(fixCoordinate(Double.valueOf(parts[i]))).append(" ");
+        }
+        return result.toString();
+    }
 }
